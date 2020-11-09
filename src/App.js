04bf7bf49b1bcda61a669f6bfe70d22991f28706
Vitaliy <v.burla96@gmail.com>
2020-11-09 20:18:04 +0200

    about section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import AboutUs from "./pages/AboutUs";
 
 function App() {
-  return (
-    <div className="App">
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <AboutUs/>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/AboutUs.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import AboutSection from "../components/AboutSection.js";
+
+const AboutUs = () => {
+    return (
+        <div>
+            <AboutSection/>
+        </div>
+    )
+
+}
+
+export default AboutUs;
